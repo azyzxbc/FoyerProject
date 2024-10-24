1e33c5eaec0eb6c0f42c623a437e02774213eb84
aziz benchikh <87549776+azyzxbc@users.noreply.github.com>
2024-10-25 00:24:32 +0100

    Update ReservationRestController.java

--- src/main/java/tn/esprit/tpfoyer/control/ReservationRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/ReservationRestController.java
@@ -14,61 +14,48 @@ import java.util.List;
 @RequestMapping("/reservation")
 public class ReservationRestController {
 
-    IReservationService reservationService;
+    private final IReservationService reservationService;
 
+    // Retrieve all reservations
     // http://localhost:8089/tpfoyer/reservation/retrieve-all-reservations
     @GetMapping("/retrieve-all-reservations")
     public List<Reservation> getReservations() {
-        List<Reservation> listReservations = reservationService.retrieveAllReservations();
-        return listReservations;
+        return reservationService.retrieveAllReservations();
     }
+
+    // Retrieve a reservation by ID
     // http://localhost:8089/tpfoyer/reservation/retrieve-reservation/8
     @GetMapping("/retrieve-reservation/{reservation-id}")
     public Reservation retrieveReservation(@PathVariable("reservation-id") String rId) {
-        Reservation reservation = reservationService.retrieveReservation(rId);
-        return reservation;
+        return reservationService.retrieveReservation(rId);
     }
 
-
-
-
-
-
-
+    // Retrieve reservations by date and status
     @GetMapping("/retrieve-reservation-date-status/{d}/{v}")
-    public List<Reservation> retrieveReservationParDateEtStatus
-            (@PathVariable("d") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date d, @PathVariable("v") boolean b) {
+    public List<Reservation> retrieveReservationParDateEtStatus(
+            @PathVariable("d") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date d,
+            @PathVariable("v") boolean b) {
         return reservationService.trouverResSelonDateEtStatus(d, b);
     }
 
-
-
-
-
-
-
-
-
-
-
+    // Add a new reservation
     // http://localhost:8089/tpfoyer/reservation/add-reservation
     @PostMapping("/add-reservation")
     public Reservation addReservation(@RequestBody Reservation r) {
-        Reservation reservation = reservationService.addReservation(r);
-        return reservation;
+        return reservationService.addReservation(r);
     }
 
+    // Remove a reservation by ID
     // http://localhost:8089/tpfoyer/reservation/remove-reservation/{reservation-id}
     @DeleteMapping("/remove-reservation/{reservation-id}")
     public void removeReservation(@PathVariable("reservation-id") String rId) {
         reservationService.removeReservation(rId);
     }
 
+    // Modify an existing reservation
     // http://localhost:8089/tpfoyer/reservation/modify-reservation
     @PutMapping("/modify-reservation")
     public Reservation modifyReservation(@RequestBody Reservation r) {
-        Reservation reservation = reservationService.modifyReservation(r);
-        return reservation;
+        return reservationService.modifyReservation(r);
     }
-
 }
