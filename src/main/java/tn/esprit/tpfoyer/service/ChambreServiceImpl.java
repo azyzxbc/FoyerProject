8585c9cc71695158c67b9b2c179b43334a8e44be
aziz benchikh <87549776+azyzxbc@users.noreply.github.com>
2024-10-25 00:26:39 +0100

    Update ChambreServiceImpl.java

--- src/main/java/tn/esprit/tpfoyer/service/ChambreServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/ChambreServiceImpl.java
@@ -1,6 +1,5 @@
 package tn.esprit.tpfoyer.service;
 
-
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,76 +8,49 @@ import tn.esprit.tpfoyer.entity.TypeChambre;
 import tn.esprit.tpfoyer.repository.ChambreRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class ChambreServiceImpl implements IChambreService {
 
-    ChambreRepository chambreRepository;
+    private final ChambreRepository chambreRepository;
 
     public List<Chambre> retrieveAllChambres() {
-        log.info("In Methodo retrieveAllChambres : ");
+        log.info("In Method retrieveAllChambres");
         List<Chambre> listC = chambreRepository.findAll();
-        log.info("Out of retrieveAllChambres : ");
-
+        log.info("Out of retrieveAllChambres");
         return listC;
     }
 
     public Chambre retrieveChambre(Long chambreId) {
-        Chambre c = chambreRepository.findById(chambreId).get();
-        return c;
+        Optional<Chambre> optionalChambre = chambreRepository.findById(chambreId);
+        if (optionalChambre.isPresent()) {
+            return optionalChambre.get();
+        } else {
+            log.warn("Chambre with ID {} not found", chambreId);
+            return null; // or throw an exception, depending on your error handling strategy
+        }
     }
 
     public Chambre addChambre(Chambre c) {
-        Chambre chambre = chambreRepository.save(c);
-        return chambre;
+        return chambreRepository.save(c); // Directly return the saved entity
     }
 
     public Chambre modifyChambre(Chambre c) {
-        Chambre chambre = chambreRepository.save(c);
-        return c;
+        return chambreRepository.save(c); // Directly return the saved entity
     }
 
     public void removeChambre(Long chambreId) {
         chambreRepository.deleteById(chambreId);
     }
 
-
-
-
-
-
-
-    public List<Chambre> recupererChambresSelonTyp(TypeChambre tc)
-    {
+    public List<Chambre> recupererChambresSelonTyp(TypeChambre tc) {
         return chambreRepository.findAllByTypeC(tc);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public Chambre trouverchambreSelonEtudiant(long cin) {
-       //
-
+    public Chambre trouverChambreSelonEtudiant(long cin) {
         return chambreRepository.trouverChselonEt(cin);
     }
 }
