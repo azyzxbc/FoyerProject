735f2ad793beb251ed56a472892daa18e3531a6a
aziz benchikh <87549776+azyzxbc@users.noreply.github.com>
2024-10-25 00:28:29 +0100

    Update FoyerServiceImpl.java

--- src/main/java/tn/esprit/tpfoyer/service/FoyerServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/FoyerServiceImpl.java
@@ -1,27 +1,38 @@
 package tn.esprit.tpfoyer.service;
 
-
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import tn.esprit.tpfoyer.entity.Foyer;
 import tn.esprit.tpfoyer.repository.FoyerRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class FoyerServiceImpl implements IFoyerService {
 
-    FoyerRepository foyerRepository;
+    private final FoyerRepository foyerRepository;
+
     public List<Foyer> retrieveAllFoyers() {
         return foyerRepository.findAll();
     }
+
     public Foyer retrieveFoyer(Long foyerId) {
-        return foyerRepository.findById(foyerId).get();
+        Optional<Foyer> optionalFoyer = foyerRepository.findById(foyerId);
+        if (optionalFoyer.isPresent()) {
+            return optionalFoyer.get();
+        } else {
+            // Handle the case when the Foyer is not found
+            // You might want to throw an exception or return null based on your error handling strategy
+            return null; // or throw new EntityNotFoundException("Foyer not found");
+        }
     }
+
     public Foyer addFoyer(Foyer f) {
         return foyerRepository.save(f);
     }
+
     public Foyer modifyFoyer(Foyer foyer) {
         return foyerRepository.save(foyer);
     }
