36cb578df72058d0889116010e93cb6fac954d54
aziz benchikh <87549776+azyzxbc@users.noreply.github.com>
2024-10-25 18:28:34 +0100

    Update ChambreRestController.java

--- src/main/java/tn/esprit/tpfoyer/control/ChambreRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/ChambreRestController.java
@@ -14,44 +14,35 @@ import java.util.List;
 public class ChambreRestController {
 
     private final IChambreService chambreService;
-
-    // Retrieve all chambres
     @GetMapping("/retrieve-all-chambres")
     public List<Chambre> getChambres() {
         return chambreService.retrieveAllChambres();
     }
-
-    // Retrieve a specific chambre by ID
     @GetMapping("/retrieve-chambre/{chambre-id}")
     public Chambre retrieveChambre(@PathVariable("chambre-id") Long chId) {
         return chambreService.retrieveChambre(chId);
     }
 
-    // Add a new chambre
     @PostMapping("/add-chambre")
     public Chambre addChambre(@RequestBody Chambre c) {
         return chambreService.addChambre(c);
     }
 
-    // Remove a chambre by ID
     @DeleteMapping("/remove-chambre/{chambre-id}")
     public void removeChambre(@PathVariable("chambre-id") Long chId) {
         chambreService.removeChambre(chId);
     }
 
-    // Modify an existing chambre
     @PutMapping("/modify-chambre")
     public Chambre modifyChambre(@RequestBody Chambre c) {
         return chambreService.modifyChambre(c);
     }
 
-    // Find chambres based on type
     @GetMapping("/trouver-chambres-selon-typ/{tc}")
     public List<Chambre> trouverChSelonTC(@PathVariable("tc") TypeChambre tc) {
         return chambreService.recupererChambresSelonTyp(tc);
     }
 
-    // Find a chambre based on student's CIN
     @GetMapping("/trouver-chambre-selon-etudiant/{cin}")
     public Chambre trouverChSelonEt(@PathVariable("cin") long cin) {
         return chambreService.trouverChambreSelonEtudiant(cin); // Corrected method name
