7733e57b0ccd7484ec6bc7e969d17e6c3ea87d37
aziz benchikh <87549776+azyzxbc@users.noreply.github.com>
2024-10-25 16:37:43 +0100

    Update UniversiteRestController.java

--- src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/UniversiteRestController.java
@@ -14,36 +14,30 @@ public class UniversiteRestController {
 
     private final IUniversiteService universiteService;
 
-    // Retrieve all universities
-    // http://localhost:8089/tpfoyer/universite/retrieve-all-universites
+    
     @GetMapping("/retrieve-all-universites")
     public List<Universite> getUniversites() {
         return universiteService.retrieveAllUniversites();
     }
 
-    // Retrieve a university by ID
-    // http://localhost:8089/tpfoyer/universite/retrieve-universite/8
+   
     @GetMapping("/retrieve-universite/{universite-id}")
     public Universite retrieveUniversite(@PathVariable("universite-id") Long uId) {
         return universiteService.retrieveUniversite(uId);
     }
 
-    // Add a new university
-    // http://localhost:8089/tpfoyer/universite/add-universite
     @PostMapping("/add-universite")
     public Universite addUniversite(@RequestBody Universite u) {
         return universiteService.addUniversite(u);
     }
 
-    // Remove a university by ID
-    // http://localhost:8089/tpfoyer/universite/remove-universite/{universite-id}
+  
     @DeleteMapping("/remove-universite/{universite-id}")
     public void removeUniversite(@PathVariable("universite-id") Long uId) {
         universiteService.removeUniversite(uId);
     }
 
-    // Modify an existing university
-    // http://localhost:8089/tpfoyer/universite/modify-universite
+    
     @PutMapping("/modify-universite")
     public Universite modifyUniversite(@RequestBody Universite u) {
         return universiteService.modifyUniversite(u);
