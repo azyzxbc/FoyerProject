0be22d1b5cefa54117c959d1a7a2fdafe232ee11
aziz benchikh <87549776+azyzxbc@users.noreply.github.com>
2024-10-25 18:28:07 +0100

    Update BlocRestController.java

--- src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
@@ -16,45 +16,37 @@ import java.util.List;
 public class BlocRestController {
 
     private final IBlocService blocService;
-
-    // Retrieve all blocs
     @GetMapping("/retrieve-all-blocs")
     @Operation(description = "WS de récupération de tous les Blocs")
     public List<Bloc> getBlocs() {
         return blocService.retrieveAllBlocs();
     }
 
-    // Retrieve a specific bloc by ID
     @GetMapping("/retrieve-bloc/{bloc-id}")
     public Bloc retrieveBloc(@PathVariable("bloc-id") Long bId) {
         return blocService.retrieveBloc(bId);
     }
 
-    // Add a new bloc
     @PostMapping("/add-bloc")
     public Bloc addBloc(@RequestBody Bloc c) {
         return blocService.addBloc(c);
     }
 
-    // Remove a bloc by ID
     @DeleteMapping("/remove-bloc/{bloc-id}")
     public void removeBloc(@PathVariable("bloc-id") Long chId) {
         blocService.removeBloc(chId);
     }
 
-    // Modify an existing bloc
     @PutMapping("/modify-bloc")
     public Bloc modifyBloc(@RequestBody Bloc c) {
         return blocService.modifyBloc(c);
     }
 
-    // Retrieve blocs without foyer
     @GetMapping("/trouver-blocs-sans-foyer")
     public List<Bloc> getBlocsWithoutFoyer() {
         return blocService.trouverBlocsSansFoyer();
     }
 
-    // Retrieve blocs by name and capacity
     @GetMapping("/get-bloc-nb-c/{nb}/{c}")
     public List<Bloc> recuperBlocsParNomEtCap(
             @PathVariable("nb") String nb,
