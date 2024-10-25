d02ed8397caeb3a459b3d946eab3badead3afedc
aziz benchikh <87549776+azyzxbc@users.noreply.github.com>
2024-10-25 18:29:11 +0100

    Update FoyerRestController.java

--- src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/FoyerRestController.java
@@ -14,31 +14,26 @@ public class FoyerRestController {
 
     private final IFoyerService foyerService;
 
-    // Retrieve all foyers
     @GetMapping("/retrieve-all-foyers")
     public List<Foyer> getFoyers() {
         return foyerService.retrieveAllFoyers();
     }
 
-    // Retrieve a foyer by ID
     @GetMapping("/retrieve-foyer/{foyer-id}")
     public Foyer retrieveFoyer(@PathVariable("foyer-id") Long fId) {
         return foyerService.retrieveFoyer(fId);
     }
 
-    // Add a new foyer
     @PostMapping("/add-foyer")
     public Foyer addFoyer(@RequestBody Foyer f) {
         return foyerService.addFoyer(f);
     }
 
-    // Remove a foyer by ID
     @DeleteMapping("/remove-foyer/{foyer-id}")
     public void removeFoyer(@PathVariable("foyer-id") Long fId) {
         foyerService.removeFoyer(fId);
     }
 
-    // Modify an existing foyer
     @PutMapping("/modify-foyer")
     public Foyer modifyFoyer(@RequestBody Foyer f) {
         return foyerService.modifyFoyer(f);
